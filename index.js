488f1d1dba2fbe11291bb77af7958082bc38fcb1
Tim Perry <pimterry@gmail.com>
2016-03-28 15:08:16 +0200

    Listen on port specified by environment

--- index.js
+++ index.js
@@ -26,4 +26,5 @@ app.get('/', function (req, res) {
     });
 });
 
-app.listen(8080, () => console.log("Listening on 8080"));
+var port = (process.env.PORT || 8080);
+app.listen(port, () => console.log(`Listening on ${port}`));
