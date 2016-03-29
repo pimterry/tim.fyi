d8194320323d0de50c1b5b0c29c5ccffe8cad0f8
Tim Perry <pimterry@gmail.com>
2016-03-29 22:04:19 +0200

    Cache rss feeds

--- components/rss-source.js
+++ components/rss-source.js
@@ -2,17 +2,29 @@
 
 var components = require("server-components");
 var domino = require("domino");
-var cache = require("memory-cache");
 var moment = require("moment");
 
 var feedparser = require('feedparser-promised');
+var cache = require("memory-cache");
+
+function getRss(url) {
+    var cachedResult = cache.get(url);
+
+    if (cachedResult) return Promise.resolve(cachedResult);
+    else {
+        return feedparser.parse(url).then((result) => {
+            cache.put(url, result, 1000 * 60* 10);
+            return result;
+        });
+    }
+}
 
 var RssSource = components.newElement();
 RssSource.createdCallback = function () {
     var url = this.getAttribute("url");
     var icon = this.getAttribute("icon");
 
-    return feedparser.parse(url).then((items) => {
+    return getRss(url).then((items) => {
         this.dispatchEvent(new domino.impl.CustomEvent('items-ready', {
             items: items.map((item) => { return {
                 details: item.title,
