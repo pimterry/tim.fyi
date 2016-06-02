153daa87d9000e5679b34ca806887968138e13ad
Tim Perry <pimterry@gmail.com>
2016-06-02 16:45:03 +0200

    Ensure scroll buttons work cross-browser with a cheeky polyfill

--- components/item-carousel/item-carousel.js
+++ components/item-carousel/item-carousel.js
@@ -10,6 +10,9 @@ readFile(__dirname + "/item-carousel.html", 'utf8').then((rawHtml) => {
     var ItemCarousel = components.newElement();
 
     ItemCarousel.createdCallback = function () {
+        // Include a tiny client-side web components polyfill, just because it makes the runtime script way nicer.
+        componentsStatic.includeScript(this.ownerDocument, "https://rawgit.com/WebReflection/document-register-element/master/build/document-register-element.js");
+        // Add some nice buttons to scroll back and forth at runtime
         componentsStatic.includeScript(this.ownerDocument, "/item-carousel.runtime.js");
 
         var items = [];
