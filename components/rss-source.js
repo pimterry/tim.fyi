7aaba2df0c741f06e478dc8a24d4d95f5ee1817a
Tim Perry <pimterry@gmail.com>
2022-11-21 14:54:45 +0100

    Replace Twitter feed content with Mastodon

--- components/rss-source.js
+++ components/rss-source.js
@@ -52,4 +52,38 @@ RssSource.createdCallback = function () {
     });
 };
 
+const RssSummarySource = components.newElement();
+RssSummarySource.createdCallback = function () {
+    const url = this.getAttribute("url");
+    const icon = this.getAttribute("icon");
+    const sourceTitle = this.getAttribute("title");
+    const useDescriptionAsTitle = this.getAttribute("descriptionAsTitle")
+
+    return getRss(url).then((feed) => {
+        this.dispatchEvent(new components.dom.CustomEvent('items-ready', {
+            items: feed.entries.map((item) => {
+                const description = truncateHtml(item.description, 110, {
+                    excludes: ['svg']
+                });
+
+                const title = useDescriptionAsTitle
+                    ? description
+                        : (item.title && item.title.match(/\w/))
+                    ? item.title
+                    : "Post on " + sourceTitle;
+
+                return {
+                    title: title,
+                    icon: icon,
+                    timestamp: moment(item.published).unix(),
+                    description: description,
+                    subtitle: `<a href="${item.link}">View on ${sourceTitle}</a>`
+                };
+            }),
+            bubbles: true
+        }));
+    });
+};
+
 components.registerElement("rss-source", { prototype: RssSource });
+components.registerElement("rss-summary-source", { prototype: RssSummarySource });
